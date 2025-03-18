a8929ec84943ec4e20f9ec5e403a67cb6cc618be
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-03-18 18:50:09 +0530

    ho

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { Avatar } from "./BlogsCard";
 import { Pencil } from "lucide-react";
 
-export const Appbar = ({ id }: { id: string }) => {
+export const Appbar = ({ id }: { id?: string }) => {
   return (
     <div className="flex justify-between items-center p-4 border-b border-slate-200">
       <div>
@@ -18,7 +18,7 @@ export const Appbar = ({ id }: { id: string }) => {
           {" "}
           <Pencil className="w-4 h-4" /> Write
         </Link>
-        <Avatar authorName={id} size="w-9 h-9" />
+        <Avatar authorName={id || ""} size="w-9 h-9" />
       </div>
     </div>
   );
